83038da05a4337a9168684aed47093cce6d43edf
05sonicblue <michaeldlesk@gmail.com>
2021-04-13 13:21:57 -0500

    Added eth chains

--- TrezorBalanceMonitoring/Program.cs
+++ TrezorBalanceMonitoring/Program.cs
@@ -21,32 +21,74 @@ namespace TrezorBalanceMonitoring
         {
             Console.WriteLine("Refreshing Trezor Info");
             var trezorFirmwareRepo = "https://github.com/trezor/trezor-firmware.git";
+            var ethChainsRepo = "https://github.com/ethereum-lists/chains.git";
+            var ethTokensRepo = "https://github.com/ethereum-lists/tokens.git";
             var trezorFirmwareWorkingDirectory = "trezorFirmware";
+            var ethChainsWorkingDirectory = "ethChains";
+            var ethTokensWorkingDirectory = "ethTokens";
             try
             {
-                LibGit2Sharp.Repository.Clone(trezorFirmwareRepo, trezorFirmwareWorkingDirectory);
-
-                if (!Directory.Exists(trezorFirmwareWorkingDirectory))
+                if (Directory.Exists(trezorFirmwareWorkingDirectory))
                 {
-                    Console.WriteLine("Error getting trezor firmware data from github.");
+                    ForceDeleteDirectory(trezorFirmwareWorkingDirectory);
                 }
-                else
-                {
-                    var monitoredCoinSection = ConfigurationManager.GetSection("Coins") as MonitoredCoinSection;
 
-                    //trezor - bitcoin types
-                    var bitCoinDirectoryInfo = new DirectoryInfo(Path.Combine(trezorFirmwareWorkingDirectory, "common", "defs", "bitcoin"));
-                    foreach (var bitcoinConfigFile in bitCoinDirectoryInfo.GetFiles("*.json"))
+                if (Directory.Exists(ethChainsWorkingDirectory))
+                {
+                    ForceDeleteDirectory(ethChainsWorkingDirectory);
+                }
+
+                if (Directory.Exists(ethTokensWorkingDirectory))
+                {
+                    ForceDeleteDirectory(ethTokensWorkingDirectory);
+                }
+                LibGit2Sharp.Repository.Clone(trezorFirmwareRepo, trezorFirmwareWorkingDirectory);
+                LibGit2Sharp.Repository.Clone(ethChainsRepo, ethChainsWorkingDirectory);
+                LibGit2Sharp.Repository.Clone(ethTokensRepo, ethTokensWorkingDirectory);
+                var monitoredCoinSection = ConfigurationManager.GetSection("Coins") as MonitoredCoinSection;
+
+                //trezor - bitcoin types
+                var bitCoinDirectoryInfo = new DirectoryInfo(Path.Combine(trezorFirmwareWorkingDirectory, "common", "defs", "bitcoin"));
+                foreach (var bitcoinConfigFile in bitCoinDirectoryInfo.GetFiles("*.json"))
+                {
+                    using (var reader = bitcoinConfigFile.OpenText())
                     {
-                        using (var reader = bitcoinConfigFile.OpenText())
+                        var configContents = reader.ReadToEnd();
+                        dynamic configData = JsonConvert.DeserializeObject(configContents);
+                        var coinName = configData.coin_name.Value;
+                        var coinId = configData.coin_shortcut.Value;
+
+                        var alreadyMonitoredConfig = monitoredCoinSection.MonitoredCoins.OfType<MonitoredCoinElement>().Where(x => x.Key == coinId).FirstOrDefault();
+                        if (alreadyMonitoredConfig == null)
                         {
-                            var configContents = reader.ReadToEnd();
-                            dynamic configData = JsonConvert.DeserializeObject(configContents);
-                            var coinName = configData.coin_name.Value;
-                            var coinId = configData.coin_shortcut.Value;
+                            var xmlDoc = new XmlDocument();
+                            xmlDoc.Load(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
+                            var nodeRegion = xmlDoc.CreateElement("add");
+                            nodeRegion.SetAttribute("key", coinId);
+                            nodeRegion.SetAttribute("coinId", coinId);
+                            nodeRegion.SetAttribute("coinName", coinName);
+                            nodeRegion.SetAttribute("address", String.Empty);
+                            xmlDoc.SelectSingleNode("//Coins").AppendChild(nodeRegion);
+                            xmlDoc.Save(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
+                        }
+                    }
+                }
+
+                //trezor - misc types
+                var miscDirectoryInfo = new DirectoryInfo(Path.Combine(trezorFirmwareWorkingDirectory, "common", "defs", "misc"));
+                foreach (var miscConfigFile in miscDirectoryInfo.GetFiles("*.json"))
+                {
+                    using (var reader = miscConfigFile.OpenText())
+                    {
+                        var configContents = reader.ReadToEnd();
+                        dynamic configData = JsonConvert.DeserializeObject(configContents);
+                        foreach (var configItem in configData)
+                        {
+                            var coinName = configItem.name.Value;
+                            var coinId = configItem.shortcut.Value;
 
                             var alreadyMonitoredConfig = monitoredCoinSection.MonitoredCoins.OfType<MonitoredCoinElement>().Where(x => x.Key == coinId).FirstOrDefault();
-                            if (alreadyMonitoredConfig==null)
+                            if (alreadyMonitoredConfig == null)
                             {
                                 var xmlDoc = new XmlDocument();
                                 xmlDoc.Load(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
@@ -59,90 +101,75 @@ namespace TrezorBalanceMonitoring
                                 xmlDoc.Save(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
                             }
                         }
+
                     }
-
-                    //TODO: trezor - ethereum types
-
-                    //trezor - misc types
-                    var miscDirectoryInfo = new DirectoryInfo(Path.Combine(trezorFirmwareWorkingDirectory, "common", "defs", "misc"));
-                    foreach (var miscConfigFile in miscDirectoryInfo.GetFiles("*.json"))
-                    {
-                        using (var reader = miscConfigFile.OpenText())
-                        {
-                            var configContents = reader.ReadToEnd();
-                            dynamic configData = JsonConvert.DeserializeObject(configContents);
-                            foreach (var configItem in configData)
-                            {
-                                var coinName = configItem.name.Value;
-                                var coinId = configItem.shortcut.Value;
-
-                                var alreadyMonitoredConfig = monitoredCoinSection.MonitoredCoins.OfType<MonitoredCoinElement>().Where(x => x.Key == coinId).FirstOrDefault();
-                                if (alreadyMonitoredConfig == null)
-                                {
-                                    var xmlDoc = new XmlDocument();
-                                    xmlDoc.Load(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
-                                    var nodeRegion = xmlDoc.CreateElement("add");
-                                    nodeRegion.SetAttribute("key", coinId);
-                                    nodeRegion.SetAttribute("coinId", coinId);
-                                    nodeRegion.SetAttribute("coinName", coinName);
-                                    nodeRegion.SetAttribute("address", String.Empty);
-                                    xmlDoc.SelectSingleNode("//Coins").AppendChild(nodeRegion);
-                                    xmlDoc.Save(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
-                                }
-                            }
-                            
-                        }
-                    }
-
-                    //trezor - nem types
-                    var nemDirectoryInfo = new DirectoryInfo(Path.Combine(trezorFirmwareWorkingDirectory, "common", "defs", "nem"));
-                    foreach (var nemConfigFile in nemDirectoryInfo.GetFiles("*.json"))
-                    {
-                        using (var reader = nemConfigFile.OpenText())
-                        {
-                            var configContents = reader.ReadToEnd();
-                            dynamic configData = JsonConvert.DeserializeObject(configContents);
-                            foreach (var configItem in configData)
-                            {
-                                var coinName = configItem.name.Value;
-                                var coinId = configItem.ticker.Value;
-
-                                var alreadyMonitoredConfig = monitoredCoinSection.MonitoredCoins.OfType<MonitoredCoinElement>().Where(x => x.Key == coinId).FirstOrDefault();
-                                if (alreadyMonitoredConfig == null)
-                                {
-                                    var xmlDoc = new XmlDocument();
-                                    xmlDoc.Load(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
-                                    var nodeRegion = xmlDoc.CreateElement("add");
-                                    nodeRegion.SetAttribute("key", coinId);
-                                    nodeRegion.SetAttribute("coinId", coinId);
-                                    nodeRegion.SetAttribute("coinName", coinName);
-                                    nodeRegion.SetAttribute("address", String.Empty);
-                                    xmlDoc.SelectSingleNode("//Coins").AppendChild(nodeRegion);
-                                    xmlDoc.Save(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
-                                }
-                            }
-
-                        }
-                    }
-
-
-
-
-
-
-                    ConfigurationManager.RefreshSection("Coins");
                 }
+
+                //trezor - nem types
+                var nemDirectoryInfo = new DirectoryInfo(Path.Combine(trezorFirmwareWorkingDirectory, "common", "defs", "nem"));
+                foreach (var nemConfigFile in nemDirectoryInfo.GetFiles("*.json"))
+                {
+                    using (var reader = nemConfigFile.OpenText())
+                    {
+                        var configContents = reader.ReadToEnd();
+                        dynamic configData = JsonConvert.DeserializeObject(configContents);
+                        foreach (var configItem in configData)
+                        {
+                            var coinName = configItem.name.Value;
+                            var coinId = configItem.ticker.Value;
+
+                            var alreadyMonitoredConfig = monitoredCoinSection.MonitoredCoins.OfType<MonitoredCoinElement>().Where(x => x.Key == coinId).FirstOrDefault();
+                            if (alreadyMonitoredConfig == null)
+                            {
+                                var xmlDoc = new XmlDocument();
+                                xmlDoc.Load(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
+                                var nodeRegion = xmlDoc.CreateElement("add");
+                                nodeRegion.SetAttribute("key", coinId);
+                                nodeRegion.SetAttribute("coinId", coinId);
+                                nodeRegion.SetAttribute("coinName", coinName);
+                                nodeRegion.SetAttribute("address", String.Empty);
+                                xmlDoc.SelectSingleNode("//Coins").AppendChild(nodeRegion);
+                                xmlDoc.Save(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
+                            }
+                        }
+
+                    }
+                }
+
+                //trezor - eth chains
+                var ethChainsDirectoryInfo = new DirectoryInfo(Path.Combine(ethChainsWorkingDirectory, "_data", "chains"));
+                foreach (var ethChainConfigFile in ethChainsDirectoryInfo.GetFiles("*.json"))
+                {
+                    using (var reader = ethChainConfigFile.OpenText())
+                    {
+                        var configContents = reader.ReadToEnd();
+                        dynamic configData = JsonConvert.DeserializeObject(configContents);
+                        var coinName = configData.name.Value;
+                        var coinId = configData.nativeCurrency.symbol.Value;
+
+                        var alreadyMonitoredConfig = monitoredCoinSection.MonitoredCoins.OfType<MonitoredCoinElement>().Where(x => x.Key == coinId && x.CoinName == coinName).FirstOrDefault();
+                        if (alreadyMonitoredConfig == null)
+                        {
+                            var xmlDoc = new XmlDocument();
+                            xmlDoc.Load(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
+                            var nodeRegion = xmlDoc.CreateElement("add");
+                            nodeRegion.SetAttribute("key", coinId);
+                            nodeRegion.SetAttribute("coinId", coinId);
+                            nodeRegion.SetAttribute("coinName", coinName);
+                            nodeRegion.SetAttribute("address", String.Empty);
+                            xmlDoc.SelectSingleNode("//Coins").AppendChild(nodeRegion);
+                            xmlDoc.Save(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
+                        }
+                    }
+                }
+
+
+                ConfigurationManager.RefreshSection("Coins");
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine("Error getting trezor firmware data from github: " + ex.Message);
             }
-
-            if (Directory.Exists(trezorFirmwareWorkingDirectory))
-            {
-                ForceDeleteDirectory(trezorFirmwareWorkingDirectory);
-            }
-
         }
 
         private static void ForceDeleteDirectory(string path)
